b77776c0db4037115ceaea0007d478c76d54f9ec
Laz <bohale@gmail.com>
2021-10-08 14:56:16 +0200

    update microservice

--- app.js
+++ app.js
@@ -1,41 +1,59 @@
-const http = require('http'); 
+const express = require('express');
 const httpProblem = require('problem-json');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express(); 
+app.use(express.json())    // <==== parse request body as JSON
+app.use(cors())
 
 const port = 3000;
 
-//accept client request 
-const server = http.createServer(function(req, res) { 
-
-    // extend error description
-    const extension = new httpProblem.Extension({
-        firstName: 'Cannot be blank.',
-        lastName: 'Cannot be blank.', 
-        studentNumber: 'Must be numeric',  
-    });
+app.post('/', (req, res) => { 
+    if ((Object.keys(req.body).length == 0) || 
+        (!req.body.firstName) ||
+        (!req.body.lastName) ||
+        (!req.body.studentNumber)) {
+            // extend error description  
+            const extension = new httpProblem.Extension({
+                invalidParms: [{
+                    firstName: 'Cannot be blank.',
+                    lastName: 'Cannot be blank.', 
+                    studentNumber: 'Must be numeric',  
+                }]
+            });
     
-    // problem details (incl. extension)
-    const doc = new httpProblem.Document({
-        type: 'https://york.ac.uk/students/registration-exception',
-        title: 'Validation error.',
-        detail: 'Student registration request parameters failed to validate.',
-        instance: 'york.ac.uk/students/registrations',
-        status: 400,
-    }, extension);
+            // problem details (incl. extension)
+            const doc = new httpProblem.Document({
+                type: 'https://york.ac.uk/students/registration-exception',
+                title: 'Validation error.',
+                detail: 'Student registration request parameters failed to validate.',
+                instance: 'york.ac.uk/students/registrations',
+                status: 400,
+            }, extension);
 
-    // send Response to client
-    res.writeHead(400, 'Student registration request parameters failed to validate.', {
-        'Content-Type': 'application/problem+json',
-        'Content-Language': 'en', 
-    });
-    res.end(JSON.stringify(doc)); 
-}); 
-
-
+            // send Response back to client
+            res.writeHead(400, 'Student registration request parameters failed to validate.', {
+                'Content-Type': 'application/problem+json',
+                'Content-Language': 'en', 
+            });
+            res.end(JSON.stringify(doc)); 
+        }  else { 
+            let Resp = {
+                "firstName": req.body.firstName,
+                "lastName": req.body.lastName,
+                "studentNumber": req.body.studentNumber
+            }
+        
+            res.set({ 
+                'Content-Type': 'application/json'
+            }); 
+            res.statusMessage = "Student details validated.";
+            res.send(JSON.stringify(Resp));
+        }
+});
+ 
 //listen for client request
-server.listen(port, function(error){
-    if (error) {
-        console.log('Something went wrong ', error);
-    } else {
-        console.log('Server listening on port ' + port);
-    }
-}); 
+app.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`); 
+});   
